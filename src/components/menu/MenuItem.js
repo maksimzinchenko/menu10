5a1355acae1344511997693fe3f0799dbf776efa
lipkij <lipkij@gmail.com>
2023-02-13 18:46:40 +0400

    fix: css fixes

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import commonClasses from '../../common.module.css';
+// import commonClasses from '../../common.module.css';
 import classes from './MenuItem.module.css';
 
 export default function MenuItem({ item, onClick }) {
   return (
-    <div className={`${commonClasses.container} ${classes.menuItem}`}>
+    <div className={`${classes.menuItem}`}>
       <img className={classes.itemImage} src={item.image} alt={item.name} />
-      <h3>{item.name}</h3>
-      <p>{item.description}</p>
-      <p>{item.price} $.</p>
-      <button onClick={() => onClick(item)}>Add to cart</button>
+      <h3 className={classes.itemTitle}>{item.name}</h3>
+      <p className={classes.itemDescription}>{item.description}</p>
+      <p className={classes.itemPrice}>{item.price} $.</p>
+      <button className={classes.addToCartButton} onClick={() => onClick(item)}>Add to cart</button>
     </div>
   );
 }
