a6ca7b8974cad11e53a2dd1d873d9a4970fe9e51
lipkij <lipkij@gmail.com>
2023-02-15 11:29:56 +0400

    add: add new logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+
+
 
 import Header from "./components/header/Header";
 import Menu from "./components/menu/Menu";
@@ -8,6 +10,66 @@ import Footer from "./components/footer/Footer";
 import { useTelegram } from "./hooks/useTelegram";
 
 import "./App.css";
+
+
+const MENUITEMS = [
+  {
+    id: "59a46597-e48d-4dc1-b014-fd3908649c6d",
+    name: "Meal 1",
+    price: 2.5,
+    is_active: true,
+    order: 1,
+    image: "../media/bakery.jpg",
+  },
+  {
+    id: "e81d9cdb-71e3-440d-9bfb-d9b97d89f957",
+    name: "Meal 2",
+    price: 3,
+    is_active: true,
+    order: 1,
+    image: "../media/breakfast.jpg",
+  },
+  {
+    id: "ed1c6231-ccb3-4527-b8a2-0bf6851676d7",
+    name: "Meal 3",
+    price: 26,
+    is_active: true,
+    order: 1,
+    image: "../media/meat.jpg",
+  },
+  {
+    id: "a2ac5f5b-3463-4c42-b0fc-020ecc3be819",
+    name: "Meal 4",
+    price: 21,
+    is_active: true,
+    order: 1,
+    image: "../media/bakery.jpg",
+  },
+  {
+    id: "5abc49cd-f1e3-470e-8d0f-670b09d16cac",
+    name: "Meal 5",
+    price: 5,
+    is_active: true,
+    order: 1,
+    image: "../media/breakfast.jpg",
+  },
+  {
+    id: "4472f08c-9c9a-4902-83aa-1247188c7eb8",
+    name: "Meal 6",
+    price: 15,
+    is_active: true,
+    order: 1,
+    image: "../media/meat.jpg",
+  },
+  {
+    id: "dc74415a-38b0-418e-bd0d-af95a4c02004",
+    name: "Meal 7",
+    price: 0.5,
+    is_active: true,
+    order: 1,
+    image: "../media/bakery.jpg",
+  },
+];
 
 const getTotalPrice = (items = []) => {
   return items.reduce((acc, item) => {
@@ -19,10 +81,35 @@ function App() {
   const [menuItems, setMenuItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [isStandalone, setStandalone] = useState(true);
-  const { tg } = useTelegram();
+  const [showCart, setShowCart] = useState(false);
+  const { tg, queryId } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+        products: cartItems,
+        queryId,
+    };
+
+    console.log('pressed send');
+    // fetch('http://85.119.146.179:8000/web-data', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(data)
+    // })
+}, [queryId, cartItems])
+
+useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData)
+    return () => {
+        tg.offEvent('mainButtonClicked', onSendData)
+    }
+}, [tg, onSendData])
 
   useEffect(() => {
     tg.ready();
+
   }, [tg]);
 
   const isTelegram = window.Telegram.WebApp.initData.length > 0;
@@ -33,64 +120,7 @@ function App() {
     } else {
       setStandalone(true);
     }
-    setMenuItems([
-      {
-        id: "59a46597-e48d-4dc1-b014-fd3908649c6d",
-        name: "Meal 1",
-        price: 2.5,
-        is_active: true,
-        order: 1,
-        image: "../media/bakery.jpg",
-      },
-      {
-        id: "e81d9cdb-71e3-440d-9bfb-d9b97d89f957",
-        name: "Meal 2",
-        price: 3,
-        is_active: true,
-        order: 1,
-        image: "../media/breakfast.jpg",
-      },
-      {
-        id: "ed1c6231-ccb3-4527-b8a2-0bf6851676d7",
-        name: "Meal 3",
-        price: 26,
-        is_active: true,
-        order: 1,
-        image: "../media/meat.jpg",
-      },
-      {
-        id: "a2ac5f5b-3463-4c42-b0fc-020ecc3be819",
-        name: "Meal 4",
-        price: 21,
-        is_active: true,
-        order: 1,
-        image: "../media/bakery.jpg",
-      },
-      {
-        id: "5abc49cd-f1e3-470e-8d0f-670b09d16cac",
-        name: "Meal 5",
-        price: 5,
-        is_active: true,
-        order: 1,
-        image: "../media/breakfast.jpg",
-      },
-      {
-        id: "4472f08c-9c9a-4902-83aa-1247188c7eb8",
-        name: "Meal 6",
-        price: 15,
-        is_active: true,
-        order: 1,
-        image: "../media/meat.jpg",
-      },
-      {
-        id: "dc74415a-38b0-418e-bd0d-af95a4c02004",
-        name: "Meal 7",
-        price: 0.5,
-        is_active: true,
-        order: 1,
-        image: "../media/bakery.jpg",
-      },
-    ]);
+    setMenuItems(MENUITEMS);
   }, [isTelegram]);
 
   // useEffect(() => {
@@ -109,7 +139,7 @@ function App() {
     } else {
       tg.MainButton.show();
       tg.MainButton.setParams({
-        text: `Buy ${getTotalPrice(cartItems)}`,
+        text: `View order ($${getTotalPrice(cartItems)})`,
       });
     }
   });
@@ -150,12 +180,12 @@ function App() {
     <div className="App">
       <Header isStandalone={isStandalone} />
       <main>
-        <Menu items={menuItems} addToCart={addToCart} />
-        <Cart
+        { !showCart && <Menu items={menuItems} addToCart={addToCart} />}
+        { showCart && <Cart
           items={cartItems}
           removeFromCart={removeFromCart}
           clearCart={clearCart}
-        />
+        />}
       </main>
       <Footer />
     </div>
@@ -163,3 +193,4 @@ function App() {
 }
 
 export default App;
+
